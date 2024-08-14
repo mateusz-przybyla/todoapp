16c190db3a2d5ea3c58e9b213c657addc47661ff
Mateusz Przybyla <mateusz.przybyla.programista@gmail.com>
2024-08-14 21:53:25 +0200

    Implemented mass assignment to TodoAppController

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield("title")</title>
+
+    @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
 </head>
 <body>
     <h1>@yield("title")</h1>

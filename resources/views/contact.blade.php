70a4c1a2010ffbf513a4b3947fdafbbd022b9059
Mateusz Przybyla <mateusz.przybyla.programista@gmail.com>
2024-08-17 14:20:30 +0200

    Created frontend for contact page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,13 +2,16 @@
 @section("title", "Contact us")
 
 @section("content")
-<form method="POST">
-    <label for="name">Name:</label><br>
-    <input type="text" id="name" name="name"><br>
-    <label for="email">Email:</label><br>
-    <input type="text" id="email" name="email"><br>
-    <label for="message">Message:</label><br>
-    <textarea id="message" name="message"></textarea><br>
-    <input type="submit" value="Send">
-</form>
+<div class="rounded main-color py-5 mb-1">
+    <h2 class="col-md-6 text-start mx-auto">Contact us</h2>
+    <form method="POST" class="col-md-6 mx-auto">
+        <label for="name" class="mt-2">Name:</label><br>
+        <input type="text" id="name" name="name" class="form-control">
+        <label for="email" class="mt-2">Email:</label><br>
+        <input type="text" id="email" name="email" class="form-control">
+        <label for="message" class="mt-2">Message:</label><br>
+        <textarea id="message" name="message"  class="form-control"></textarea>
+        <input type="submit" value="Send" class="btn btn-primary mt-3">
+    </form>
+</div>
 @endsection

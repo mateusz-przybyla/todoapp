3cd76c19b86fa45595bb185ec339ae3d98479936
Mateusz Przybyla <mateusz.przybyla.programista@gmail.com>
2024-08-20 10:43:11 +0200

    Storing messages in a database with validation rules, success alert

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,11 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Message;
 use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
+    protected $validationRules = ["name" => "required", "email" => ["required", "email"], "message" => "required"];
+
     public function index(){
         return view('contact');
+    }
+
+    public function store(Request $request)
+    {
+        //Log::info($request);
+
+        $validatedData = $request->validate($this->validationRules);
+
+        Message::create($validatedData);
+
+        return redirect()->route("contact")->with("status", "Your message has been sent!");
     }
 }
